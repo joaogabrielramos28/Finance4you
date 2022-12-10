e49e54d407a002f71c0809bc81111734560db44f
João Gabriel Ramos <71793869+joaogabrielramos28@users.noreply.github.com>
2022-12-09 22:35:56 -0300

    ✅ tests: home screen

--- src/__tests__/screens/Home.spec.tsx
+++ src/__tests__/screens/Home.spec.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 import { render, fireEvent, waitFor } from "@testing-library/react-native";
-import { renderHook } from "@testing-library/react-hooks";
 import { Home } from "../../screens";
-import { getMonthsName } from "../../utils/formatMonth";
 import { AppProvider } from "../../context/";
 import { format, subMonths } from "date-fns";
 import { ptBR } from "date-fns/locale";
-import { debug } from "react-native-reanimated";
 
 jest.mock("@invertase/react-native-apple-authentication", () => {
   return {
@@ -23,12 +20,21 @@ const prevMonth = format(subMonths(new Date(), 1), "MMMM/yyyy", {
 });
 
 describe("Home Screen", () => {
-  it("should render the current month and year by default", () => {
+  it("Should be render the home title", async () => {
     const { getByText } = render(<Home />, {
       wrapper: AppProvider,
     });
-
-    expect(getByText(currentMonth)).toBeTruthy();
+    await waitFor(() => {
+      expect(getByText("Resumo de gastos")).toBeTruthy();
+    });
+  });
+  it("should render the current month and year by default", async () => {
+    const { getByText } = render(<Home />, {
+      wrapper: AppProvider,
+    });
+    await waitFor(() => {
+      expect(getByText(currentMonth)).toBeTruthy();
+    });
   });
 
   it("should can back to the prev month", async () => {
@@ -44,17 +50,34 @@ describe("Home Screen", () => {
     });
   });
   it("Shouldn't be able to go to the next month when selected month is current", async () => {
-    const { getByTestId, debug } = render(<Home />, {
+    const { getByTestId } = render(<Home />, {
       wrapper: AppProvider,
     });
 
     const nextMonthButton = getByTestId("next-month-button");
-    debug(nextMonthButton);
 
     await waitFor(() => {
       expect(nextMonthButton.props.accessibilityState["disabled"]).toEqual(
         true
       );
+    });
+  });
+  it("should show message if don't have transactions", async () => {
+    const { getByText } = render(<Home />, {
+      wrapper: AppProvider,
+    });
+
+    await waitFor(() => {
+      expect(getByText("Nenhuma movimentação")).toBeTruthy();
+    });
+  });
+  it("should render graph ", async () => {
+    const { getByText } = render(<Home />, {
+      wrapper: AppProvider,
+    });
+
+    await waitFor(() => {
+      expect(getByText(/gastos de/i)).toBeTruthy();
     });
   });
 });
